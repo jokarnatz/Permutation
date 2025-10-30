0037eefaea71949515271af832a880996f64ea71
Johannes Karnatz <641_000004905@tln.bfw-hamburg.de>
2025-10-30 14:37:06 +0100

    better console message/fully-shown equation with substituted variables + result

--- permutation.py
+++ permutation.py
@@ -17,17 +17,18 @@ def get_input(prompt, datatype):
             print(f"An error occurred: {e}")
 
 def create_vars(usr_choice):
-    p = [1,2,3,4,5,6,7,8,9]
+    DIV: str = chr(247)
+    p: list[int] = [1,2,3,4,5,6,7,8,9]
     a, b, c, d, e, f, g, h, i = p
     var: list = [a,b,c,d,e,f,g,h,i]
     TARGET_VALUE: int = usr_choice
-    MAX_COUNTS = 300_000
-    return var, TARGET_VALUE, MAX_COUNTS
+    MAX_COUNTS: int = 300_000
+    return var, TARGET_VALUE, MAX_COUNTS, DIV
 
 def calculate_expression(var: list):
     return var[0] + 13 * var[1] / var[2] + var[3] / 12 * var[4] - var[5] - 11 / var[6] * var[7] / var[8] - 10
 
-def calculate_permutation(var: list, TARGET_VALUE: int, MAX_COUNTS):
+def calculate_permutation(var: list, TARGET_VALUE: int, MAX_COUNTS: int, DIV: str):
     count = 0
     while True:
         shuffle(var)
@@ -38,12 +39,14 @@ def calculate_permutation(var: list, TARGET_VALUE: int, MAX_COUNTS):
             print(f"Reached maximum calculations without result.\nExiting..")  
             break
         elif mathm_expression == TARGET_VALUE:
-            print(f"Found a fitting variation in run {count}:\n{' '.join(map(str,var))}")
-            print(f"{mathm_expression:g}")
+            print(f"Found a fitting variation in run {count:,.0f}:\n{' '.join(map(str,var))}")
+            print(f"[{var[0]}] + 13 * [{var[1]}] {DIV} [{var[2]}] + [{var[3]}] {DIV} 12 * [{var[4]}] - [{var[5]}] - 11 {DIV} [{var[6]}] * [{var[7]}] {DIV} [{var[8]}] - 10 = {mathm_expression:g}")
             break
-            
-                
-if __name__ == '__main__':
+
+def main():
     usr_choice = get_input("Choose a result number. Should be a small number: ", int)
-    var, TARGET_VALUE, MAX_COUNTS = create_vars(usr_choice)
-    calculate_permutation(var, TARGET_VALUE, MAX_COUNTS)
+    var, TARGET_VALUE, MAX_COUNTS, DIV = create_vars(usr_choice)
+    calculate_permutation(var, TARGET_VALUE, MAX_COUNTS, DIV)
+
+if __name__ == '__main__':
+    main()
